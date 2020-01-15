76a0206e943d278c84d9ef8eff596d5c7a982565
Martin Siron <msiron@lbl.gov>
2020-01-15 13:15:19 -0800

    - added easy filter feature (front-end) - starting to implement login feature (back-end)

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 import pandas as pd
+import datetime
+from config import Config
+from data.form import LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 
@@ -22,18 +28,18 @@ fd = FundingResources(df)
 @app.route('/resources')
 def index():
     resources = fd.funding_resources
-    return render_template('resources.html', resources=resources)
+    return render_template('resources.html', resources=resources, datetime=datetime)
 
 @app.route('/keyword/', methods=['GET'])
 def findby():
     if request.method == 'GET':
         keyword, value = request.args.get('keyword',"description"),request.args.get('value',"")
         resources = fd.find_by(keyword,value)
-        return render_template('resources.html', resources=resources)
+        return render_template('resources.html', resources=resources, datetime=datetime)
 @app.route('/keyword/<keyword>/<value>', methods=['GET'])
 def findby_url(keyword, value):
     resources = fd.find_by(keyword, value)
-    return render_template('resources.html', resources=resources)
+    return render_template('resources.html', resources=resources, datetime=datetime)
 
 @app.route('/welcome/')
 def welcome():
@@ -55,7 +61,17 @@ def applyfilter():
         filter_criteria = res_fund
     resources = fd.filter_by_type(funding_for,filter_criteria)
 
-    return render_template('resources.html', resources=resources)
+    return render_template('resources.html', resources=resources, datetime=datetime)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 
