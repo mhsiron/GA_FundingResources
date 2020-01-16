31870f5f5c63506669e6c911a59360bd1a08d68d
Martin Siron <msiron@lbl.gov>
2020-01-16 11:21:15 -0800

    404, 500 error

--- app/routes.py
+++ app/routes.py
@@ -235,6 +235,14 @@ def manage():
         resources = FundingResources.query.filter_by(user_id=current_user.id)
         return render_template('resources.html', resources=resources,
                                datetime=datetime, editable=True)
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 
